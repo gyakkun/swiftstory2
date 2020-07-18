6f04639bf7b8d96bcbf9cd6d3c15599b112f0b0e
gyakkun <xiaowufeixia@gmail.com>
2020-07-19 00:48:17 +0800

    Initial commit.

---
+++ swiftstory2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swiftstory2
+//
+//  Created by gyakkun on 2020/7/13.
+//  Copyright © 2020 gyakkun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
