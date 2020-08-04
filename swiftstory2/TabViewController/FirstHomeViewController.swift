5d90667a536dd485539b69bcf39a1c89619c7661
gyakkun <xiaowufeixia@gmail.com>
2020-08-04 11:24:15 +0800

    Move button to mine view.
    Add gray view.

--- swiftstory2/TabViewController/FirstHomeViewController.swift
+++ swiftstory2/TabViewController/FirstHomeViewController.swift
@@ -14,21 +14,8 @@ class FirstHomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print(kScreenW,kScreenH)
-        view.addSubview(button)
     }
 
-    private lazy var button : UIButton = {
 
-        let btn=UIButton.init(type:.roundedRect)
-        btn.frame = CGRect(x: 10, y: 300, width: kScreenW-20, height: 100)
-        btn.backgroundColor = UIColor.green
-        btn.addTarget(self, action: #selector(btnClick), for: .touchUpInside)
-        return btn
-    }()
-
-    @objc private func btnClick(){
-        print(123)
-    }
 }
 
--- swiftstory2/TabViewController/FourthMineViewController.swift
+++ swiftstory2/TabViewController/FourthMineViewController.swift
@@ -12,10 +12,32 @@ class FourthMineViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.addSubview(button)
+        view.addSubview(grayView)
+        print(kScreenW,kScreenH)
         // Do any additional setup after loading the view.
     }
-    
+
+    private lazy var button : UIButton = {
+
+        let btn=UIButton.init(type:.roundedRect)
+        btn.frame = CGRect(x: 10, y: 300, width: kScreenW-20, height: 100)
+        btn.backgroundColor = UIColor.green
+        btn.addTarget(self, action: #selector(btnClick), for: .touchUpInside)
+        return btn
+    }()
+
+    @objc private func btnClick(){
+        print("[btnClick] btn clicked.")
+        grayView.alpha = 1.0
+    }
+
+    private lazy var grayView : UIView = {
+        let gray = UIView.init(frame: self.view.bounds)
+        gray.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5)
+        gray.alpha = 0.0
+        return gray
+    }()
 
     /*
     // MARK: - Navigation
