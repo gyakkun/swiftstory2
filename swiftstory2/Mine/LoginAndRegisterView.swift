8fd9d71140175e83840b4a8dc94c7dc65dc0ca2d
gyakkun <xiaowufeixia@gmail.com>
2020-10-20 15:22:25 +0800

    Add comments.

--- swiftstory2/Mine/LoginAndRegisterView.swift
+++ swiftstory2/Mine/LoginAndRegisterView.swift
@@ -9,26 +9,25 @@
 import UIKit
 
 class LoginAndRegisterView: UIView {
-
+    // 顶部标题
     @IBOutlet weak var topTitle: UILabel!
-
+    // 手机号输入view
     @IBOutlet weak var phoneView: UIView!
-
+    // 手机号输入文本框
     @IBOutlet weak var phoneTextField: UITextField!
-
+    // 验证码输入view
     @IBOutlet weak var smsTextCodeView: UIView!
-
+    // 验证码输入文本框
     @IBOutlet weak var smsTextField: UITextField!
-
+    // 注册登录按钮
     @IBOutlet weak var registerButton: UIButton!
-
+    // 注册类型选择
     @IBOutlet weak var loginTypeView: UIView!
-
+    // 同意复选框
     @IBOutlet weak var agreeToggle: UIButton!
-    
+    // 查看协议view
     @IBOutlet weak var viewAgreementView: UIView!
-    
-    
+
     override func layoutSubviews() {
         setUI()
 
